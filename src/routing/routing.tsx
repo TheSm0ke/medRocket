a30d8d223b3e43349e6cc32936831292e22fc22c
TheSm0ke <danilsmith448@gmail.com>
2023-06-07 23:16:20 +0300

    Всегда подгружаются пользователи

--- src/routing/routing.tsx
+++ src/routing/routing.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Catalog from "../pages/catalog";
 import Favorites from "../pages/favorites";
-import ErrorPage from "../pages/error/error";
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path="*" element={<ErrorPage />} />
-      <Route path="/catalog" element={<Catalog />} />
+      <Route path="*" element={<Catalog />} />
       <Route path="/favorites" element={<Favorites />} />
     </Routes>
   );
