528ecce36435893fca0a2bb073d77c3de460bbff
TheSm0ke <danilsmith448@gmail.com>
2023-06-08 15:16:04 +0300

    Закончил с header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,39 @@
 import { Link } from "react-router-dom";
+import "./header.scss";
+import { useState } from "react";
 
 const Header = () => {
+  const [backgroudCatalog, setBackgroundCatalog] = useState({});
+  const [backgroudFavorites, setBackgroundFavorites] = useState({});
+
+  const handleClick = (itCatalog: boolean) => {
+    if (itCatalog) {
+      setBackgroundCatalog({ backgroundColor: "#EDF9FF", color: "#117DC1" });
+      setBackgroundFavorites({ backgroundColor: "white", color: "black" });
+    } else {
+      setBackgroundCatalog({ backgroundColor: "white", color: "black" });
+      setBackgroundFavorites({ backgroundColor: "#EDF9FF", color: "#117DC1" });
+    }
+  };
+
   return (
     <div className="header">
-      <div className="catalog">
-        <Link to="/catalog">Каталог</Link>
-      </div>
-      <div className="favorites">
-        <Link to="/favorites">Избранное</Link>
-      </div>
+      <Link
+        id="catalog"
+        onClick={() => handleClick(true)}
+        to="/catalog"
+        style={backgroudCatalog}
+        className="header-link">
+        Каталог
+      </Link>
+      <Link
+        id="favorites"
+        onClick={() => handleClick(false)}
+        className="header-link"
+        style={backgroudFavorites}
+        to="/favorites">
+        Избранное
+      </Link>
     </div>
   );
 };
